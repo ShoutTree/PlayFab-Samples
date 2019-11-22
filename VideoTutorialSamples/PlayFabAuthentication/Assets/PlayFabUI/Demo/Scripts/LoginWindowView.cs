570427cad512e86b00639a5d5950a8bf732ccc92
ShoutTree <zengxiaoqi@gmail.com>
2019-11-22 09:09:16 +0800

    some more commits

--- VideoTutorialSamples/PlayFabAuthentication/Assets/PlayFabUI/Demo/Scripts/LoginWindowView.cs
+++ VideoTutorialSamples/PlayFabAuthentication/Assets/PlayFabUI/Demo/Scripts/LoginWindowView.cs
@@ -113,6 +113,48 @@ public class LoginWindowView : MonoBehaviour {
 
         //Start the authentication process.
         _AuthService.Authenticate();
+
+        //test setting statistics.
+        CreatePlayerAndPopulateLeaderboard();
+    }
+
+    // Note: This is a recursive function. Invoke it initially with no parameter
+    public void CreatePlayerAndPopulateLeaderboard(int playerIndex = 5)
+    {
+        if (playerIndex <= 0) return;
+        const string leaderboardName = "tournamentScore_manual";
+        PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest
+        {
+            CustomId = playerIndex.ToString(),
+            CreateAccount = true
+        }, result => OnLoggedIn(result, playerIndex, leaderboardName), FailureCallback);
+    }
+
+    private void OnLoggedIn(LoginResult loginResult, int playerIndex, string leaderboardName)
+    {
+        Debug.Log("Player has successfully logged in with " + loginResult.PlayFabId);
+        PlayFabClientAPI.UpdatePlayerStatistics(new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate> {
+            new StatisticUpdate {
+                StatisticName = leaderboardName,
+                Value = playerIndex + 100
+            }
+        }
+        }, result => OnStatisticsUpdated(result, playerIndex), FailureCallback);
+    }
+
+    private void OnStatisticsUpdated(UpdatePlayerStatisticsResult updateResult, int playerIndex)
+    {
+        Debug.Log("Successfully updated player statistic");
+        // Recursively invoke for next player
+        CreatePlayerAndPopulateLeaderboard(playerIndex - 1);
+    }
+
+    private void FailureCallback(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your API call. Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
     }
 
     void RunLogTest()
